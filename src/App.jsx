1d5a79ab337b174d535a46e059ffbe4fe85b5062
Roy Cuadra <roycuadra16@gmail.com>
2023-05-29 01:20:59 +0800

    initial

--- src/App.jsx
+++ src/App.jsx
@@ -1,75 +1,71 @@
 import Result from "./Components/Result";
 import axios from "axios";
-import { useState } from "react";
-import { useEffect } from "react";
-const APIURL = "https://api.themoviedb.org/3/discover/movie?sort_by=popularity.desc&api_key=04c35731a5ee918f014970082a0088b1&page=1";
-const SEARCHAPI = "https://api.themoviedb.org/3/search/movie?&api_key=04c35731a5ee918f014970082a0088b1&query=";
+import { useState, useEffect } from "react";
+import "./App.css";
+
+const APIURL =
+  "https://api.themoviedb.org/3/discover/movie?sort_by=popularity.desc&api_key=04c35731a5ee918f014970082a0088b1&page=1";
+const SEARCHAPI =
+  "https://api.themoviedb.org/3/search/movie?&api_key=04c35731a5ee918f014970082a0088b1&query=";
+
 function App() {
   const [movies, setMovies] = useState([]);
   const [search, setSearch] = useState("");
 
   const changeTheSearch = (event) => {
-    // console.log(event.target.value);
     setSearch(event.target.value);
-  }
+  };
 
   const getAllMovies = () => {
-    axios.get(APIURL)
-      .then(
-        (response) => {
-          console.log(response.data.results)
-          setMovies(response.data.results);
-        }
-      )
-      .catch(
-        (error) => {
-          console.log(error)
-        }
-      )
-  }
+    axios
+      .get(APIURL)
+      .then((response) => {
+        console.log(response.data.results);
+        setMovies(response.data.results);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  };
 
   const getSearchedMovies = () => {
-    // console.log(SEARCHAPI + search)
-    axios.get(
-      SEARCHAPI + search
-    )
-      .then(
-        (response) => {
-          console.log(response.data.results)
-          setMovies(response.data.results);
-        }
-      )
-      .catch(
-        (error) => { 
-          console.log(error);
-        }
-      )
-  }
+    axios
+      .get(SEARCHAPI + search)
+      .then((response) => {
+        console.log(response.data.results);
+        setMovies(response.data.results);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  };
 
-  useEffect(
-    () => {
-      setMovies([]);
-      // console.log("Hello");
-      if (search === "") {
-        getAllMovies();
-      } else {
-        getSearchedMovies();
-      }
-    },
-    [search]
-  )
+  useEffect(() => {
+    setMovies([]);
+    if (search === "") {
+      getAllMovies();
+    } else {
+      getSearchedMovies();
+    }
+  }, [search]);
 
   return (
-    <div className="max-w-[1240px] shadow-xl min-h-[400px] mx-auto p-3">
-      <input type="search" value={search} onChange={changeTheSearch} className="w-full border border-black rounded text-slate-700 p-4" />
-      {
-        movies.length === 0
-          ?
-          <div className="text-3xl text-center mt-2"> Loading... </div>
-          :
-          <Result movies={movies} />
-
-      }
+    <div className="app-container">
+      <h1 className="title">Movieluxe</h1>
+      <div className="search-container">
+        <input
+          type="search"
+          value={search}
+          onChange={changeTheSearch}
+          className="search-input"
+          placeholder="Search movies..."
+        />
+      </div>
+      {movies.length === 0 ? (
+        <div className="loading-text">Loading...</div>
+      ) : (
+        <Result movies={movies} />
+      )}
     </div>
   );
 }
--- src/Components/Result.jsx
+++ src/Components/Result.jsx
@@ -1,28 +1,41 @@
-import React from 'react'
+import React, { useEffect } from 'react';
+import AOS from 'aos';
+import 'aos/dist/aos.css';
 
 export default function Result(props) {
-    const boxes = props.movies.map(
-        (item,index) => {
-            return <Box key={index} image={item.poster_path} title={item.original_title} rating={item.vote_average} />
-        }
-    )
-    return (
-        <div className='w-full grid md:grid-cols-4 gap-5'>
-            {boxes}
-        </div>
-    )
-}
+  useEffect(() => {
+    AOS.init({
+      delay: 1000 // Set delay to 1000 milliseconds (1 second)
+    });
+  }, []);
 
+  const boxes = props.movies.map((item, index) => {
+    return (
+      <Box
+        key={index}
+        image={item.poster_path}
+        title={item.original_title}
+        rating={item.vote_average}
+      />
+    );
+  });
+
+  return (
+    <div className='result-container' data-aos='fade-up'>
+      {boxes}
+    </div>
+  );
+}
 
 const Box = (props) => {
-    const IMGPATH = "https://image.tmdb.org/t/p/w1280";
-    return (
-        <div className='shadow min-h-[200px] mt-3 pb-1'>
-            <img src={IMGPATH + props.image} alt="" className='w-full' />
-            <div className='flex justify-between px-2 items-center'>
-                <span className='text-2xl'>{props.title}</span>
-                <span className='text-xl text-yellow-500 font-bold'>{props.rating}</span>
-            </div>
-        </div>
-    )
-}
+  const IMGPATH = 'https://image.tmdb.org/t/p/w1280';
+  return (
+    <div className='result-box shadow'>
+      <img src={IMGPATH + props.image} alt='' className='result-image' />
+      <div className='result-details'>
+        <span className='result-title'>{props.title}</span>
+        <span className='result-rating'>{props.rating}</span>
+      </div>
+    </div>
+  );
+};
